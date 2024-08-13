c694bb4b55fcbcacc743918e58a25b56fc952d1e
INGOSVALDORR <INGOsvaldoRR@outlook.com>
2024-08-12 21:14:40 -0600

    diseño buscador de video
    diseño buscador de los videos usando la data api

--- innovatubeangular/src/app/app.component.ts
+++ innovatubeangular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'innovatubeangular';
+  constructor(private router: Router) {}
+
+  goToInicioSesion() {
+    this.router.navigate(['/inicio-sesion']).catch(error => {
+      console.error('Error during navigation:', error);
+    });
+  }
+
+  goToHome() {
+    this.router.navigate(['/']).catch(error => {
+      console.error('Error during navigation:', error);
+    });
+  }
 }
